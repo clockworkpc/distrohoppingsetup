caa6d5b9add01f5078f65d55e65bc2e37dfaedba
alexander <clockworkpc@gmail.com>
2020-12-28 10:56:43 +1100

    first commit

---
+++ 00_basic_packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Setting up my required packages."
+echo "Need sudo..."
+sudo echo "Now I have sudo."
+
+sudo pacman -Syu git
+git clone https://aur.archlinux.org/yay-git.git
+cd ~/yay-git
+makepkg -si
+
+yay -S brave
